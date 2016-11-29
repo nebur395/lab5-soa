957480a99398b182faafbc5005b68bb6d2443e4c
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-11-29 16:24:03 +0100

    Add limit query search

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.*;
+
 
 @Controller
 public class SearchController {
@@ -23,6 +25,17 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        // save the "limit" restriction of the RequestParam  into a [limitCount] variable
+        int indexMax = q.indexOf("max");
+        String limitString = q.substring(indexMax);
+        String[] limit = limitString.split(":");
+        limitString = limit[1];
+        Integer limitCount = Integer.parseInt(limitString);
+        // save the query of the RequestParam into a [q] variable
+        q = q.substring(0,indexMax);
+        Map<String,Object> headers = new HashMap<String,Object>();
+        headers.put("CamelTwitterKeywords",q);
+        headers.put("CamelTwitterCount",limitCount);
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
     }
 }
